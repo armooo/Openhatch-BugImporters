8b77c7b553a8ad88556b121c491d9f2f569d1814
Asheesh Laroia <asheesh@asheesh.org>
2011-10-27 15:10:14 -0400

    Moving bug importers into separate Python package

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+version = '0.11.09'
+
+setup(name="bugimporters",
+      version=version,
+      author='OpenHatch development team <http://lists.openhatch.org/mailman/listinfo/devel>',
+      license='AGPLv3',
+      packages=find_packages(),
+      install_requires=[])
