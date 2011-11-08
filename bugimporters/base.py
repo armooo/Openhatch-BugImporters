a2aa81a0955fb7b92d858d12f3b0c7865c25ce2a
Daenyth <Daenyth+git@gmail.com>
2011-11-07 19:49:03 -0500

    base.py: Remove unused import

--- bugimporters/base.py
+++ bugimporters/base.py
@@ -16,7 +16,6 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import twisted.web.client
-import logging
 
 from mysite.customs.models import TrackerModel
 from mysite.search.models import Bug
